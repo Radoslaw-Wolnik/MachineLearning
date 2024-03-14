104c4030256f07b0c811ae8ee0ed260d6d7ab659
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-03-14 17:49:04 +0100

    completed implementation

---
+++ 04_Naive_Bayes/src/NaiveBayes.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class NaiveBayes:
+
+    def __init__(self):
+        self._mean = None
+        self._var = None
+        self._priors = None
+        self._classes = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        # initial - mean, variance, priors
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for c in self._classes:
+            X_c = X[c==y]
+            self._mean[c,:] = X_c.mean(axis=0)
+            self._var[c,:] = X_c.var(axis=0)
+            self._priors[c] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return y_pred
+
+    def _predict(self, x):
+        posteriors = []
+
+        for idx, c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            class_conditional = np.sum(np.log(self._probability_density_function(idx, x)))
+            posterior = prior + class_conditional
+            posteriors.append(posterior)
+
+        return self._classes[np.argmax(posteriors)]
+
+    def _probability_density_function(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(- (x - mean) ** 2 / (2*var))
+        denominator = np.sqrt(2* np.pi * var)
+        return numerator / denominator
--- 04_Naive_Bayes/src/main.py
+++ 04_Naive_Bayes/src/main.py
@@ -2,19 +2,29 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
-# from import
+from NaiveBayes import NaiveBayes
 # best plot colours:
 # royalblue, lightcoral, salmon, gold, forestgreen, limegreen, mediumseagreen, springgreen, teal, cornflowerblue, navy, darkorchid, purple
+
+
+def accuracy(y_true, y_predicted):
+    acc = np.sum(y_true == y_predicted) / len(y_true)
+    return acc
 
 
 if __name__ == '__main__':
     print("Naive Bayes")
 
-    bc = datasets.load_breast_cancer()
-    X, y = bc.data, bc.target
-
-    fig = plt.figure(figsize=(8, 6))
-    plt.scatter(X[:, 0], y, color='royalblue', marker='o', s=20, alpha=0.5)
-    plt.show()
+    X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes= 2, random_state=123)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+    # fig = plt.figure(figsize=(8, 6))
+    # plt.scatter(X[:, 0], y, color='royalblue', marker='o', s=20, alpha=0.5)
+    # plt.show()
+
+    nb = NaiveBayes()
+    nb.fit(X_train, y_train)
+    predictions = nb.predict(X_test)
+
+    print(f'Naive Bayes classification accuracy: {accuracy(y_test, predictions)}')
