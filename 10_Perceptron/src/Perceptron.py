ed884a5fe51dd97c181c5c4fb38aa33d8647168f
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-03-14 18:01:54 +0100

    renamed directories

---
+++ 10_Perceptron/src/Perceptron.py
@@ -0,0 +1,10 @@
+# one unit of neural network
+# simplified model of biological neuron
+# simulates the behavior of one cell
+
+class Perceptron:
+
+    def __init__(self):
+        pass
+
+    
--- 10_Perceptron/src/main.py
+++ 10_Perceptron/src/main.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
-# from import
+from Perceptron import Perceptron
 # best plot colours:
 # royalblue, lightcoral, salmon, gold, forestgreen, limegreen, mediumseagreen, springgreen, teal, cornflowerblue, navy, darkorchid, purple
 
