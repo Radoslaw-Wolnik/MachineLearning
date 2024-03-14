7dcd84d516ec11803e409f95fe23af2943b68e88
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-03-14 14:33:59 +0100

    completed

---
+++ 02_Linear_regression/src/LinearRegression.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class LinearRegression:
+
+    def __init__(self, learning_rate=0.001, n_iterations=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iterations
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        # initial paramiters
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features) # for each component we put zero
+        self.bias = 0
+        # gradient decetns method
+        for _ in range(self.n_iters):
+            # approximation of y = weigts * x + bias
+            y_predicted = np.dot(X, self.weights) + self.bias
+            # derivative of weight
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
+            # derivative of bias
+            db = (1/n_samples) * np.sum(y_predicted - y)
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        y_predicted = np.dot(X, self.weights) + self.bias
+        return y_predicted
--- 02_Linear_regression/src/main.py
+++ 02_Linear_regression/src/main.py
@@ -1,6 +1,41 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from LinearRegression import LinearRegression
 
-
+def mse(y_true, y_predicted):
+    # cost function Means Square Error
+    return np.mean((y_true - y_predicted) **2)
 
 
 if __name__ == '__main__':
     print("Linear regression")
+    # split data to training and samples
+    X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+    fig = plt.figure(figsize = (8, 6))
+    plt.scatter(X[:, 0], y, color='blue', marker='o', s=20)
+    plt.show()
+
+    # print(X_train.shape)
+    # print(y_train.shape)
+
+    regressor = LinearRegression(learning_rate=0.01)
+    regressor.fit(X_train, y_train)
+    predicted = regressor.predict(X_test)
+
+    # to measure how our model performance we use Means Square Error
+    mse_value = mse(y_test, predicted)
+    print(mse_value)
+
+    # plot
+    y_predict_line = regressor.predict(X)
+    cmap = plt.get_cmap('viridis')
+    fig = plt.figure(figsize=(8,6))
+    m1 = plt.scatter(X_train, y_train, color=cmap(0.9), s=10)
+    m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
+    plt.plot(X, y_predict_line, color='black', linewidth=2, label="Prediction")
+    plt.show()
+
