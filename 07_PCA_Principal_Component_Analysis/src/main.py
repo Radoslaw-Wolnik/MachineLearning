9e5ee492eecb453ba998106e42a445a78a87a664
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-03-15 14:31:54 +0100

    completed implementation

--- 07_PCA_Principal_Component_Analysis/src/main.py
+++ 07_PCA_Principal_Component_Analysis/src/main.py
@@ -2,19 +2,36 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
-# from import
+# from matplotlib import colormaps
+from PCA import PCA
 # best plot colours:
 # royalblue, lightcoral, salmon, gold, forestgreen, limegreen, mediumseagreen, springgreen, teal, cornflowerblue, navy, darkorchid, purple
 
 
 if __name__ == '__main__':
-    print("Project name")
+    print("Principal Component Analysis")
+    # should be nr 08 - swap with SVM
 
-    bc = datasets.load_breast_cancer()
-    X, y = bc.data, bc.target
+    iris = datasets.load_iris()
+    X, y = iris.data, iris.target
 
-    fig = plt.figure(figsize=(8, 6))
-    plt.scatter(X[:, 0], y, color='royalblue', marker='o', s=20, alpha=0.5)
+    # fig = plt.figure(figsize=(8, 6))
+    # plt.scatter(X[:, 0], y, color='royalblue', marker='o', s=20, alpha=0.5)
+    # plt.show()
+
+    # project the data onto 2 primary principal components
+    pca = PCA(2)
+    pca.fit(X)
+    X_projected = pca.transform(X)
+
+    print(f'Shape of X: {X.shape}')
+    print(f'Shape of projected X: {X_projected.shape}')
+
+    x1 = X_projected[:, 0]
+    x2 = X_projected[:, 1]
+
+    plt.scatter(x1, x2, c=y, edgecolors='none', alpha=0.8, cmap='viridis')
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+    plt.colorbar()
     plt.show()
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
